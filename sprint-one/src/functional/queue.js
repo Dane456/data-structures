e7a1848dd73d9313317b72a6cbf8d5c8ecbedced
Dane456 <dane.e.jordan@gmail.com>
2016-06-08 16:05:27 -0700

    Refactored queue to pass last test case of queue specific behavior

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -9,14 +9,17 @@ var Queue = function() {
   someInstance.enqueue = function(value) {
     var key = someInstance.size();
     storage[key] = value;
+    console.log('enqueue: ' + JSON.stringify(storage));
   };
 
   someInstance.dequeue = function() {
     var value = storage[0];
-    delete storage[0];
     for (var key in storage) {
-      key--;
+      //shift values in storage
+      storage[key] = storage[Number(key) + 1];
     }
+    delete storage[someInstance.size() - 1];
+    console.log('dequeue: ' + value + ' ' + JSON.stringify(storage));
     return value;
   };
 
@@ -26,3 +29,9 @@ var Queue = function() {
 
   return someInstance;
 };
+
+//{0 : 'value0', 1: 'value1', 2: 'value2'}
+
+//{0: 'value1', 1: 'value2', 2: 'value2'}
+
+//{0: 'value1', 1: 'value2'}
