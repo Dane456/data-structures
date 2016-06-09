90ab5d8e07efd17d3bd314a463ff8f57668412da
Dane456 <dane.e.jordan@gmail.com>
2016-06-09 11:29:22 -0700

    linked list complete

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,13 +3,48 @@ var LinkedList = function() {
   list.head = null;
   list.tail = null;
 
+  //list
+  //{head: new Node(), tail: Node()}
+  //{head: {
+//    value: 'value1'
+//    next: new Node()
+//  }}
   list.addToTail = function(value) {
+    var node = new Node(value);
+    
+    //Initialization
+    if (list.head === null) {
+      list.head = node;
+    } else {
+      //list.head has number
+      current = list.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+    list.tail = node;
   };
 
   list.removeHead = function() {
+    var head = list.head;
+    delete list.head;
+    list.head = head.next;
+    return head.value;
   };
 
   list.contains = function(target) {
+    current = list.head;
+    if (current.value === target) {
+      return true;
+    }
+    while (current.next) {
+      current = current.next;
+      if (current.value === target) {
+        return true;
+      }
+    }
+    return false;
   };
 
   return list;
@@ -27,3 +62,5 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+//O(n)
