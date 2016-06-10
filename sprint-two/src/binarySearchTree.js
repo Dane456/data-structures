170dd08de1bb12dcc77e4b561c64fad6a47217fd
Dane Jordan <dane.e.jordan@gmail.com>
2016-06-10 10:45:06 -0700

    first commit

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,21 @@
 var BinarySearchTree = function(value) {
+	this.value = value;
+	this.left = undefined;
+	this.right = undefined;
 };
 
 
+BinarySearchTree.prototype.insert = function(first_argument) {
+	// body...
+};
+
+BinarySearchTree.prototype.contains = function(first_argument) {
+	// body...
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(first_argument) {
+	// body...
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
